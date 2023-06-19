3f42791ac29d6920b65814a5f9d98754bd9400d0
fmdfazil <137099228+fmdfazil@users.noreply.github.com>
2023-06-19 22:46:03 +0530

    Add files via upload

---
+++ streamlit_data_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title('Data Chat')
+st.sidebar.markdown('### Select Dataset')
+st.sidebar.selectbox("Available Datasets",['charging vio','vio based stations','Demand based stations','Merket based stations'],index=0)
+user_input = st.text_input('User:', value='', max_chars=500)
+submit_button = st.button('Send')
