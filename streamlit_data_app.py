4d9c06a718200f054b3de761664bf03b4ff961bd
fmdfazil <137099228+fmdfazil@users.noreply.github.com>
2023-06-20 11:01:08 +0530

    Update streamlit_data_app.py

--- streamlit_data_app.py
+++ streamlit_data_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title('Data Chat')
+st.title('skynet Chat')
 st.sidebar.markdown('### Select Dataset')
 st.sidebar.selectbox("Available Datasets",['charging vio','vio based stations','Demand based stations','Merket based stations'],index=0)
 user_input = st.text_input('User:', value='', max_chars=500)
