eb092b0a8f63de8dee8cadb0cadb50f89b0df48d
fmdfazil <137099228+fmdfazil@users.noreply.github.com>
2023-06-28 13:14:43 +0530

    Update streamlit_data_app.py with openai model

--- streamlit_data_app.py
+++ streamlit_data_app.py
@@ -1,6 +1,36 @@
+# Iteration 1
+#import streamlit as st
+#st.title('Data Chat')
+#st.sidebar.markdown('### Select Dataset')
+#st.sidebar.selectbox("Available Datasets",['charging vio','vio based stations','Demand based stations','Merket based stations'],index=0)
+#user_input = st.text_input('User:', value='', max_chars=500)
+#submit_button = st.button('Send')
+
+import pandas as pd
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+from pandasai.llm.open_assistant import OpenAssistant
 import streamlit as st
+
+# Defining model
+
+OPENAI_API_KEY = "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"
+llm = OpenAI(api_token=OPENAI_API_KEY)
+
+pandas_ai = PandasAI(llm)
+
+
+# load data
+df_long = pd.read_csv(r'C:\Users\Mohamed.Fazil\dummy_data_long.csv')
+
+
 st.title('Data Chat')
 st.sidebar.markdown('### Select Dataset')
 st.sidebar.selectbox("Available Datasets",['charging vio','vio based stations','Demand based stations','Merket based stations'],index=0)
-user_input = st.text_input('User:', value='', max_chars=500)
-submit_button = st.button('Send')
+
+user_prompt = st.text_input("Enter your prompt")
+
+
+if st.button("Submit"):
+    response = pandas_ai.run(df_long, prompt=user_prompt, is_conversational_answer=False)
+    st.write("Response:", response)
