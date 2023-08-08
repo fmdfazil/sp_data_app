e3ad279d6d622a7dfd706c3f5e50e25148c5bb30
fmdfazil <137099228+fmdfazil@users.noreply.github.com>
2023-08-08 11:55:36 +0530

    Update streamlit_data_app.py

--- streamlit_data_app.py
+++ streamlit_data_app.py
@@ -1,48 +1,48 @@
 # Iteration 1
-#import streamlit as st
-#st.title('Data Chat')
-#st.sidebar.markdown('### Select Dataset')
-#st.sidebar.selectbox("Available Datasets",['charging vio','vio based stations','Demand based stations','Merket based stations'],index=0)
-#user_input = st.text_input('User:', value='', max_chars=500)
-#submit_button = st.button('Send')
-
-
-import os
-import tempfile
-import subprocess
-import sys
-import pandas as pd
-from pandasai import PandasAI
-from pandasai.llm.openai import OpenAI
-from pandasai.llm.open_assistant import OpenAssistant
 import streamlit as st
-
-# Set the cache directory
-cache_dir = os.path.join(tempfile.gettempdir(), "pandasai_cache")
-os.makedirs(cache_dir, exist_ok=True)
-
-# Upgrade pip and install packages
-def install_packages():
-    with open('requirements.txt') as f:
-        packages = f.read().splitlines()
-    for package in packages:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install_packages()
-
-# Defining model
-OPENAI_API_KEY = "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"
-llm = OpenAI(api_token=OPENAI_API_KEY)
-pandas_ai = PandasAI(llm)
-
-# load data
-df_long = pd.read_csv('dummy_data_long.csv')
-
 st.title('Data Chat')
 st.sidebar.markdown('### Select Dataset')
-st.sidebar.selectbox("Available Datasets", ['charging vio', 'vio based stations', 'Demand based stations', 'Merket based stations'], index=0)
+st.sidebar.selectbox("Available Datasets",['charging vio','vio based stations','Demand based stations','Merket based stations'],index=0)
+user_input = st.text_input('User:', value='', max_chars=500)
+submit_button = st.button('Send')
 
-user_prompt = st.text_input("Enter your prompt")
+
+# import os
+# import tempfile
+# import subprocess
+# import sys
+# import pandas as pd
+# from pandasai import PandasAI
+# from pandasai.llm.openai import OpenAI
+# from pandasai.llm.open_assistant import OpenAssistant
+# import streamlit as st
+
+# # Set the cache directory
+# cache_dir = os.path.join(tempfile.gettempdir(), "pandasai_cache")
+# os.makedirs(cache_dir, exist_ok=True)
+
+# # Upgrade pip and install packages
+# def install_packages():
+#     with open('requirements.txt') as f:
+#         packages = f.read().splitlines()
+#     for package in packages:
+#         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# install_packages()
+
+# # Defining model
+# OPENAI_API_KEY = "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"
+# llm = OpenAI(api_token=OPENAI_API_KEY)
+# pandas_ai = PandasAI(llm)
+
+# # load data
+# df_long = pd.read_csv('dummy_data_long.csv')
+
+# st.title('Data Chat')
+# st.sidebar.markdown('### Select Dataset')
+# st.sidebar.selectbox("Available Datasets", ['charging vio', 'vio based stations', 'Demand based stations', 'Merket based stations'], index=0)
+
+# user_prompt = st.text_input("Enter your prompt")
 
 if st.button("Submit"):
     response = pandas_ai.run(df_long, prompt=user_prompt, is_conversational_answer=False)
