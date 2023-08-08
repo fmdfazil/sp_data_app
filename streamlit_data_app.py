bada873a35591a9ebba17c2329ca745c5519aaca
fmdfazil <137099228+fmdfazil@users.noreply.github.com>
2023-08-08 12:07:33 +0530

    Update streamlit_data_app.py

--- streamlit_data_app.py
+++ streamlit_data_app.py
@@ -1,10 +1,10 @@
-# Iteration 1
-import streamlit as st
-st.title('Data Chat')
-st.sidebar.markdown('### Select Dataset')
-st.sidebar.selectbox("Available Datasets",['charging vio','vio based stations','Demand based stations','Merket based stations'],index=0)
-user_input = st.text_input('User:', value='', max_chars=500)
-submit_button = st.button('Send')
+# # Iteration 1
+# import streamlit as st
+# st.title('Data Chat')
+# st.sidebar.markdown('### Select Dataset')
+# st.sidebar.selectbox("Available Datasets",['charging vio','vio based stations','Demand based stations','Merket based stations'],index=0)
+# user_input = st.text_input('User:', value='', max_chars=500)
+# submit_button = st.button('Send')
 
 
 # import os
@@ -44,6 +44,45 @@ submit_button = st.button('Send')
 
 # user_prompt = st.text_input("Enter your prompt")
 
-if st.button("Submit"):
-    response = pandas_ai.run(df_long, prompt=user_prompt, is_conversational_answer=False)
-    st.write("Response:", response)
+# if st.button("Submit"):
+#     response = pandas_ai.run(df_long, prompt=user_prompt, is_conversational_answer=False)
+#     st.write("Response:", response)
+
+
+
+import streamlit as st
+import pandas as pd
+
+# Create the dataframe
+data = {
+    'brand_name': ['benz', 'bmw', 'audi', 'jaguar', 'toyota'],
+    'sales_volume': [100, 30, 80, 50, 500]
+}
+df = pd.DataFrame(data)
+
+
+##Iteration 3
+# Streamlit UI
+st.title('Data Chat')
+st.sidebar.markdown('### Select Dataset')
+selected_dataset = st.sidebar.selectbox("Available Datasets", ['charging vio', 'vio based stations', 'Demand based stations', 'brand sales'], index=0)
+user_input = st.text_input('User:', value='', max_chars=500)
+submit_button = st.button('Send')
+
+# Handle user input
+if submit_button:
+    if selected_dataset == 'charging vio':
+        st.write("No data found")
+    elif selected_dataset == 'vio based stations':
+        st.write("No data found")
+    elif selected_dataset == 'Demand based stations':
+        st.write("No data found")
+    elif selected_dataset == 'brand sales':
+        if user_input == 'whats the most selling brand':
+            most_selling_brand = df.loc[df['sales_volume'].idxmax(), 'brand_name']
+            st.write(f"The most selling brand is {most_selling_brand}")
+        elif user_input == 'what the sales by brand':
+            st.write(df)
+        else:
+            st.write("No data found")
+
